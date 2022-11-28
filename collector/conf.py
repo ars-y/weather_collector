2740ed62075c068358c36dc33d8e6375557e7367
ArslanYadov <arsyy90@gmail.com>
2022-11-28 23:05:44 +0300

    change engine to postgresql

--- collector/conf.py
+++ collector/conf.py
@@ -1,12 +1,20 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-#engine = create_engine('sqlite:///:memory:', echo=True)
-engine = create_engine('sqlite:///sqlite.db', echo = True)
+url_object = URL.create(
+    'postgresql',
+    username=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    database=os.getenv('DB_NAME')
+)
+engine = create_engine(url_object, echo=True)
+engine.connect()
 
 
 WEATHER_API_URL: str = os.getenv('WEATHER_API_URL')
